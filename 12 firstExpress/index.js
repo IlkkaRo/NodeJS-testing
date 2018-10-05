3c0e8c7a8e82f49acf373d5f1183d82ca8cf3cc2
Roinisto Ilkka <s1800092@student.businesscollege.fi>
2018-10-05 10:52:42 +0300

    pic name

--- 12 firstExpress/index.js
+++ 12 firstExpress/index.js
@@ -4,8 +4,11 @@ const express = require('express');
 const app     = express();
 const port    = 3000;
 
-app.get('/', (req, res) => res.send('Express to the world!'));
+app.get('/', (req, res) => res.send('Express to the world! Try /pic!'));
 
 app.get('/pic', (req, res) => res.sendFile(__dirname + '/hold.jpg'));
 
-app.listen(port, () => console.log(`Example listening to port ${port}!`));
+app.listen(port, () => console.log(`Server serves ${port}!`));
+
+//app.listen(function(){console.log(`Server serves ${this.address().port}`);});
+//when there is no designated port
