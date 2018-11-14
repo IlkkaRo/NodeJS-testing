ad88febf4bddb6153186881d3f509c25b264a57a
Roinisto Ilkka <s1800092@student.businesscollege.fi>
2018-11-14 12:55:54 +0200

    RestApi start

---
+++ RestApi/gameApi.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path    = require('path');
+const fs      = require('fs');
+
+const routes  = require('express').Router();
+
+module.exports  = () => {
+
+
+  return routes;
+}
---
+++ RestApi/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const path    = require('path');
+const http    = require('http');
+const cors    = require('cors');
+
+const express = require('express');
+const app     = require('app');
+
+const port    = 3000;
+const host    = 'localhost';
+
+const server  = http.createServer(app);
+
+app.use(cors());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+/* eslint-disable no-console*/
+server.listen(port,host, () => console.log(`Server at ${port}`));
